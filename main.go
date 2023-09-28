3655b9e8eeef747bdfa04e566c5fe5fbdce1efcd
praivesi <praivesi@gmail.com>
2023-09-28 23:26:29 +0900

    feat: add Seoul Open API request for people fluctuation data

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
-	"github.com/tmc/langchaingo/llms/openai"
 )
 
 // Handler
@@ -30,15 +28,31 @@ func main() {
 	// e.Logger.Fatal(e.Start(":1323"))
 
 	// OpenAI code snippet
-	llm, err := openai.New()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// llm, err := openai.New()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	prompt := "What's your model name?"
-	completion, err := llm.Call(context.Background(), prompt)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(completion)
+	// prompt := "What's your model name?"
+	// completion, err := llm.Call(context.Background(), prompt)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(completion)
+
+	// Seoul Open API code snippet
+	// url := "http://openapi.seoul.go.kr:8088/" + os.Getenv("SEOUL_OPEN_API_KEY") + "/json/citydata_ppltn/1/5/광화문·덕수궁"
+
+	// resp, err := http.Get(url)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer resp.Body.Close()
+
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(string(body))
 }
