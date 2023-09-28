52bd6262ccc6b9b5e07f63b59978dd2457dde0de
praivesi <praivesi@gmail.com>
2023-09-28 17:31:18 +0900

    feat: add GO echo server

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+// Handler
+func handleHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", handleHome)
+	e.Logger.Fatal(e.Start(":1323"))
+}
