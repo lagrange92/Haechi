d6492769f72b511659ea45ef8b1ad98c209efeb9
praivesi <praivesi@gmail.com>
2023-09-28 18:46:18 +0900

    feat: add langchaingo

--- go.mod
+++ go.mod
@@ -3,10 +3,15 @@ module github.com/lagrange92/Haechi
 go 1.21.1
 
 require (
+	github.com/dlclark/regexp2 v1.8.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/echo/v4 v4.11.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.2 // indirect
+	github.com/tmc/langchaingo v0.0.0-20230927190705-e3b6d21f5662 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/tmc/langchaingo/llms/openai"
 )
 
 // Handler
@@ -11,9 +15,30 @@ func handleHome(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
-func main() {
-	e := echo.New()
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
-	e.GET("/", handleHome)
-	e.Logger.Fatal(e.Start(":1323"))
+func main() {
+	// Echo code snippet
+	// e := echo.New()
+
+	// e.GET("/", handleHome)
+	// e.Logger.Fatal(e.Start(":1323"))
+
+	// OpenAI code snippet
+	llm, err := openai.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prompt := "What's your model name?"
+	completion, err := llm.Call(context.Background(), prompt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(completion)
 }
