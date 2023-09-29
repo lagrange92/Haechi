d452703c7d7073c9bcac9059a3b914d7c3f65919
praivesi <praivesi@gmail.com>
2023-09-29 18:30:59 +0900

    feat.web: add Naver HeatMap function into main map

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <p>Welcome to Haechi!!!</p>
     <NaverMap />
   </div>
 </template>
@@ -17,12 +16,9 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
--- web/src/components/NaverMap.vue
+++ web/src/components/NaverMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="naver_map" style="width: 100%; height: 400px"></div>
+  <div id="naver_map" style="width: 100%; height: 100vh"></div>
 </template>
 
 <script>
@@ -13,10 +13,43 @@ export default {
     initMap() {
       const naver = window.naver;
       const mapOptions = {
-        center: new naver.maps.LatLng(37.3595704, 127.105399),
-        zoom: 10,
+        center: new naver.maps.LatLng(37.541, 126.986),
+        zoom: 12,
       };
-      new naver.maps.Map("naver_map", mapOptions);
+      const naverMap = new naver.maps.Map("naver_map", mapOptions);
+
+      var heatmapData = [
+        new naver.maps.visualization.WeightedLocation(37.541, 126.986, 0.1),
+        new naver.maps.visualization.WeightedLocation(37.551, 126.976, 0.2),
+        new naver.maps.visualization.WeightedLocation(37.561, 126.966, 0.3),
+        new naver.maps.visualization.WeightedLocation(37.571, 126.956, 0.4),
+        new naver.maps.visualization.WeightedLocation(37.581, 126.946, 0.5),
+        new naver.maps.visualization.WeightedLocation(37.531, 126.936, 0.6),
+        new naver.maps.visualization.WeightedLocation(37.521, 126.926, 0.7),
+        new naver.maps.visualization.WeightedLocation(37.511, 126.916, 0.8),
+        new naver.maps.visualization.WeightedLocation(37.501, 126.906, 0.9),
+        new naver.maps.visualization.WeightedLocation(37.541, 126.996, 1.0),
+        // new naver.maps.visualization.WeightedLocation(37.541, 126.986, 1),
+        // new naver.maps.visualization.WeightedLocation(37.551, 126.976, 1),
+        // new naver.maps.visualization.WeightedLocation(37.561, 126.966, 1),
+        // new naver.maps.visualization.WeightedLocation(37.571, 126.956, 0.8),
+        // new naver.maps.visualization.WeightedLocation(37.581, 126.946, 0.7),
+        // new naver.maps.visualization.WeightedLocation(37.531, 126.936, 1),
+        // new naver.maps.visualization.WeightedLocation(37.521, 126.926, 0.9),
+        // new naver.maps.visualization.WeightedLocation(37.511, 126.916, 0.8),
+        // new naver.maps.visualization.WeightedLocation(37.501, 126.906, 0.8),
+        // new naver.maps.visualization.WeightedLocation(37.541, 126.996, 1.0),
+      ];
+
+      naver.maps.onJSContentLoaded = function () {
+        new naver.maps.visualization.HeatMap({
+          map: naverMap,
+          data: heatmapData,
+          opacity: 1,
+          radius: 50,
+          colorMap: naver.maps.visualization.SpectrumStyle.RAINBOW,
+        });
+      };
     },
   },
 };
